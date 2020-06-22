5713fbb1e2a008e911856ad907f1d9e56e2d4158
Vandan Pawar <vandan.pawar@perennialsys.com>
2020-06-22 17:39:40 +0530

    Initial commit

---
+++ JenkinsDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by Perennial Systems on 22/06/20.
+//  Copyright © 2020 Vandan Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Viewdidload")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
