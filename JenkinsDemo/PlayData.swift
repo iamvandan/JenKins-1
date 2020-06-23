15990d5d5d696a42965df3d58d37574a3f68dbf6
Vandan Pawar <vandan.pawar@perennialsys.com>
2020-06-23 18:35:48 +0530

    Unit test added

---
+++ JenkinsDemo/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  JenkinsDemo
+//
+//  Created by Perennial Systems on 23/06/20.
+//  Copyright © 2020 Vandan Pawar. All rights reserved.
+//
+
+import UIKit
+
+class PlayData: NSObject {
+
+}
