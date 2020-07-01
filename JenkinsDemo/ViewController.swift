4b878100ab21d885ecff09de5b8175bdbe955544
Vandan Pawar <vandan.pawar@perennialsys.com>
2020-07-01 17:27:41 +0530

    Temp commit-3

--- JenkinsDemo/ViewController.swift
+++ JenkinsDemo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        print("View appear okok")
+        print("View appear okokk")
     }
 
 
